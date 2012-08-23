d7cbbdf876a62cba1f6f7a0581263958f4da1935
Aaron Echols <atechols@gmail.com>
2012-08-23 07:14:30 -0700

    USB: fix instabilities to mounting of USB storage devices
    Change-Id: Ic549f8bf6adc0c4a63584521c15aeb049905fda3

--- arch/arm/mach-tegra/board-star.c
+++ arch/arm/mach-tegra/board-star.c
@@ -577,8 +577,6 @@ static struct tegra_usb_platform_data tegra_udc_pdata = {
 		.xcvr_setup		= 15,
 		.xcvr_lsfslew		= 2,
 		.xcvr_lsrslew		= 2,
-		.xcvr_setup_offset	= 0,
-		.xcvr_use_fuses		= 1,
 	},
 };
 
@@ -602,6 +600,8 @@ static struct tegra_usb_platform_data tegra_ehci1_utmi_pdata = {
 		.xcvr_setup		= 15,
 		.xcvr_lsfslew		= 2,
 		.xcvr_lsrslew		= 2,
+		.xcvr_setup_offset	= 0,
+		.xcvr_use_fuses		= 1,
 	},
 };
 
